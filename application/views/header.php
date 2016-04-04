12faf2695c2fa941419ccbf3af4ecf977c702c42
unknown <fbashir@fbashir-lt.folio3pk.com>
2016-04-04 18:39:34 +0500

    Revert "Admin basic theme setup"
    This reverts commit 07d3d61039f695ebf7c5dd589e54a371161e619a.

--- application/views/header.php
+++ application/views/header.php
@@ -1,63 +1,64 @@
-<header class="main-header">
-        <!-- Logo -->
-        <a href="index2.html" class="logo">
-          <!-- mini logo for sidebar mini 50x50 pixels -->
-          <span class="logo-mini"><b>iCP</b></span>
-          <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><img src="<?php echo asset_img('logo.png');?>" class="logo icon" alt="User Image" /></span>
-        </a>
-        <!-- Header Navbar: style can be found in header.less -->
-        <nav class="navbar navbar-static-top" role="navigation">
-          <!-- Sidebar toggle button-->
-          <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+<header class="header">
+    <a href="<?php echo base_url();?>" class="logo">
+        <!-- Add the class icon to your logo image or logo icon to add the margining -->
+
+       <!-- <img src="<?php //-->echo asset_img('logo-small.png');?>" class="logo icon" alt="WooUp Admin" /> -->
+	   WooUp Admin
+    </a>
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top" role="navigation">
+        <!-- Sidebar toggle button-->
+        <a href="#" class="navbar-btn sidebar-toggle" data-toggle="offcanvas" role="button">
             <span class="sr-only">Toggle navigation</span>
-          </a>
-          <div class="navbar-custom-menu">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </a>
+        <div class="navbar-right">
             <ul class="nav navbar-nav">
-              <?php //$this->load->view('header_notifications'); ?>
-              <!-- User Account: style can be found in dropdown.less -->
-              <li class="dropdown user user-menu">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <img src="<?php echo asset_img('user2-160x160.jpg');?>" class="user-image" alt="User Image">
-                  <span class="hidden-xs">Alexander Pierce</span>
-                </a>
-                <ul class="dropdown-menu">
-                  <!-- User image -->
-                  <li class="user-header">
-                    <img src="<?php echo asset_img('user2-160x160.jpg');?>" class="img-circle" alt="User Image">
-                    <p>
-                      Admin
-                      <small><?php echo date('F j,Y');?></small>
-                    </p>
-                  </li>
-                  <!-- Menu Body -->
-                  <!-- <li class="user-body">
-                    <div class="col-xs-4 text-center">
-                      <a href="#">Followers</a>
-                    </div>
-                    <div class="col-xs-4 text-center">
-                      <a href="#">Sales</a>
-                    </div>
-                    <div class="col-xs-4 text-center">
-                      <a href="#">Friends</a>
-                    </div>
-                  </li> -->
-                  <!-- Menu Footer-->
-                  <li class="user-footer">
-                    <div class="pull-left">
-                      <a href="<?php echo site_url('admin/profile/setting') ?>" class="btn btn-default btn-flat">Profile</a>
-                    </div>
-                    <div class="pull-right">
-                      <a href="<?php echo base_url();?>/index.php/auth/logout" class="btn btn-default btn-flat">Sign out</a>
-                    </div>
-                  </li>
-                </ul>
-              </li>
-              <!-- Control Sidebar Toggle Button -->
-              <!-- <li>
-                <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
-              </li> -->
+                <!-- User Account: style can be found in dropdown.less -->
+                <li class="dropdown user user-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <i class="glyphicon glyphicon-user"></i>
+                        <span>Admin <i class="caret"></i></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <!-- User image -->
+                        <!--<li class="user-header bg-light-blue">
+                            <img src="<?php echo asset_img('avatar3.png');?>" class="img-circle" alt="User Image" />
+                            <p>
+                                Admin
+<!--                                <small>Member since Nov. 2012</small>-->
+                            <!--</p>
+                        </li>-->
+                        <!-- Menu Body -->
+<!--
+                        <li class="user-body">
+                            <div class="col-xs-4 text-center">
+                                <a href="#">Followers</a>
+                            </div>
+                            <div class="col-xs-4 text-center">
+                                <a href="#">Sales</a>
+                            </div>
+                            <div class="col-xs-4 text-center">
+                                <a href="#">Friends</a>
+                            </div>
+                        </li>
+-->
+                        <!-- Menu Footer-->
+                        <li class="user-footer">
+<!--
+                            <div class="pull-left">
+                                <a href="#" class="btn btn-default btn-flat">Profile</a>
+                            </div>
+-->
+                            <div class="pull-right">
+                                <a href="<?php echo base_url();?>/index.php/auth/logout" class="btn btn-default btn-flat">Sign Out</a>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
             </ul>
-          </div>
-        </nav>
-      </header>
+        </div>
+    </nav>
+</header>
--- application/views/login.php
+++ application/views/login.php
@@ -1,79 +1,58 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>iCannPay | Log in</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <!-- Bootstrap 3.3.5 -->
-    <link rel="stylesheet" href="<?php echo asset_css('bootstrap.min.css');?>">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="<?php echo asset_url('plugins/font-awesome/font-awesome.min.css');?>">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="<?php echo asset_url('plugins/ionicon/ionicons.min.css');?>">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="<?php echo asset_css('AdminLTE.css');?>">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="<?php echo asset_url('plugins/iCheck/flat/blue.css');?>">
+<html class="bg-light-gray">
+    <head>
+        <meta charset="UTF-8">
+        <title>Dashboard</title>
+        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+        <link href="<?php echo asset_css('bootstrap.min.css'); ?>" rel="stylesheet" type="text/css" />
+        <link href="<?php echo asset_css('font-awesome.min.css'); ?>" rel="stylesheet" type="text/css" />
+        <!-- Theme style -->
+        <link href="<?php echo asset_css('AdminLTE.css'); ?>" rel="stylesheet" type="text/css" />
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body class="hold-transition login-page">
-    <div class="login-box">
-      <div class="login-logo">
-          <img src="<?php echo asset_img('logo.png');?>" class="logo icon" alt="User Image" />
-      </div><!-- /.login-logo -->
-      <div class="login-box-body">
-        <p class="login-box-msg">Sign in to start your session</p>
-        <form action="<?php echo base_url(); ?>index.php/auth/check_database" method="post">
-          <div class="form-group has-feedback">
-            <input type="email" name="username" class="form-control" placeholder="Email">
-            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-          </div>
-          <div class="form-group has-feedback">
-            <input type="password" name="password" class="form-control" placeholder="Password">
-            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-          </div>
-          <div class="row">
-            <div class="col-xs-8">
-              <div class="checkbox icheck">
-                <label>
-                  <input type="checkbox"> Remember Me
-                </label>
-              </div>
-            </div><!-- /.col -->
-            <div class="col-xs-4">
-              <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-            </div><!-- /.col -->
-          </div>
-        </form>
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body class="bg-light-gray">
 
-        
-        <a href="#">I forgot my password</a><br>
-        
-      </div><!-- /.login-box-body -->
-    </div><!-- /.login-box -->
+        <div class="form-box" id="login-box">
+             <img src="<?php echo asset_img('logo-small.png');?>" class="logo icon" style="margin: -29px 0px 30px 109px;"alt="User Image" />
+            <br/>
+            <div class="header">Sign In</div>
+            <form action="<?php echo base_url(); ?>index.php/auth/check_database" method="post">
+                <div class="body bg-gray">
+                    <?php
+                    if (isset($error)) {
+                        echo '<div class="callout callout-danger">
+                                ' . $error . '
+                            </div>';
+                    }
+                    ?>
+                    <div class="form-group">
+                        <input type="text" name="username" class="form-control" placeholder="User ID"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="password" class="form-control" placeholder="Password"/>
+                    </div>          
+                    <div class="form-group">
+                        <input type="checkbox" name="remember_me"/> Remember me
+                    </div>
+                </div>
+                <div class="footer">                                                               
+                    <button type="submit" class="btn bg-olive btn-block">Sign me in</button>  
 
-    <!-- jQuery 2.1.4 -->
-    <script src="<?php echo asset_url('plugins/jQuery/jQuery-2.1.4.min.js');?>"></script>
-    <!-- Bootstrap 3.3.5 -->
-    <script src="<?php echo asset_js('bootstrap.min.js');?>"></script>
-    <!-- iCheck -->
-    <script src="<?php echo asset_url('plugins/iCheck/icheck.min.js');?>"></script>
-    <script>
-      $(function () {
-        $('input').iCheck({
-          checkboxClass: 'icheckbox_square-blue',
-          radioClass: 'iradio_square-blue',
-          increaseArea: '20%' // optional
-        });
-      });
-    </script>
-  </body>
+<!-- <p><a href="#">I forgot my password</a></p>
+
+<a href="register.html" class="text-center">Register a new membership</a> -->
+                </div>
+            </form>
+        </div>
+
+        <script src="<?php echo asset_js('jquery.min.js'); ?>"></script>
+        <script src="<?php echo asset_js('bootstrap.min.js'); ?>" type="text/javascript"></script>
+
+    </body>
 </html>
